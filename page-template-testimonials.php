fc56276a58e3906869e95ab25c05b4e26fe443de
Matteo Cirami <matteo@mscwebservices.net>
2020-04-28 12:14:20 -0500

    added testimonials ACF fields

--- page-template-testimonials.php
+++ page-template-testimonials.php
@@ -10,103 +10,240 @@
 /*
 
 */
-get_header(); ?>
+get_header();
+
+?>
 <?php get_template_part( 'content', 'page-header' ); ?>
 
-	<section class="row video_text_slider">
-		<div class="col-12">
-			<div class="container">
-				<div id="carouselVideoText" class="carousel slide carousel-fade" data-ride="carousel">
-					<ol class="carousel-indicators">
-						<li data-target="#carouselVideoText" data-slide-to="0" class="active"></li>
-						<li data-target="#carouselVideoText" data-slide-to="1"></li>
-					</ol>
-					<div class="carousel-inner">
-						<div class="carousel-item active">
-							<article class="row">
-								<div class="col-6">
-									<img class="video_open" src="<?php echo bloginfo( 'template_url' ); ?>/images/video-placeholder.jpg" data-video="https://www.youtube.com/watch?v=GjeRrJljrHs&list=RDGjeRrJljrHs"/>
-									<!--<img class="video_open" src="" alt="<?php /*echo esc_attr( $image['alt'] ); */?>" data-video="<?php /*the_field('video_link'); */?>">-->
-								</div>
-								<div class="col-6">
-									<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi error magnam nulla, omnis porro velit vero vitae! Asperiores distinctio impedit nam numquam odio quaerat sint veniam. Aliquam quam rerum veniam."</p>
-									<h4>Gopal</h4>
-								</div>
-							</article>
-						</div>
-						<div class="carousel-item">
-							<article class="row">
-								<div class="col-6">
-									<img class="video_open" src="<?php echo bloginfo( 'template_url' ); ?>/images/video-placeholder-slider.jpg" data-video="https://www.youtube.com/watch?v=GjeRrJljrHs&list=RDGjeRrJljrHs"/>
-									<!--<img class="video_open" src="" alt="<?php /*echo esc_attr( $image['alt'] ); */?>" data-video="<?php /*the_field('video_link'); */?>">-->
-								</div>
-								<div class="col-6">
-									<p>“ Alan I love the topics you're covering, and I love the convenience of the video library. I've been doing the exercises for just a few days, and I can tell you I am already becoming more self-aware. I know I have a lot of negative self-beliefs to overcome, but I definitely feel like this is working - and it's only been a few days. I can't wait to see where my life will be in the future! ”</p>
-									<h4>Bobby</h4>
-								</div>
-							</article>
+<?php
+
+$videoTextSlider = get_field('text_only_slider');
+
+if($videoTextSlider) :
+
+?>
+	<?php if ( have_rows( 'text_only_slider' ) ) : ?>
+
+		<?php while ( have_rows( 'text_only_slider' ) ) : the_row(); ?>
+
+			<section class="row text_slider">
+				<div class="col-12">
+					<div class="container">
+						<div id="carouselText" class="carousel slide carousel-fade" data-ride="carousel">
+
+							<?php if ( have_rows( 'text_only_slide' ) ) : $count = 0;?>
+
+								<div class="carousel-inner">
+
+									<?php while ( have_rows( 'text_only_slide' ) ) : the_row(); $count++; ?>
+
+
+										<div class="carousel-item <?php if ($count == 1) echo 'active'; ?>">
+
+											<article class="row">
+												<div class="col-12 col-md-8 mx-auto">
+
+													<div class="text_box d-flex align-content-center">
+														<p class="m-auto"><?php the_sub_field('slide_text');?></p>
+													</div>
+													<h4 class="d-block mt-4"><?php the_sub_field('slide_name');?></h4>
+												</div>
+											</article>
+
+										</div><!-- carousel-item" -->
+
+									<?php endwhile; ?>
+
+								</div><!--carousel-inner  -->
+
+								<a class="carousel-control-prev" href="#carouselText" role="button" data-slide="prev">
+									<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+									<span class="sr-only">Previous</span>
+								</a>
+								<a class="carousel-control-next" href="#carouselText" role="button" data-slide="next">
+									<span class="carousel-control-next-icon" aria-hidden="true"></span>
+									<span class="sr-only">Next</span>
+								</a>
+							<?php endif; ?>
 						</div>
 					</div>
 				</div>
-			</div>
-		</div>
-	</section>
+			</section>
 
-	<section class="row text_slider">
-		<div class="col-12">
-			<div class="container">
-				<div id="carouselText" class="carousel slide carousel-fade" data-ride="carousel">
-					<div class="carousel-inner">
-						<div class="carousel-item active">
-							<article class="row d-flex">
-								<div class="col-5 mr-5 ml-auto">
-									<div class="text_box d-flex">
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi error magnam nulla, omnis porro velit vero vitae! Asperiores distinctio impedit nam numquam odio quaerat sint veniam. Aliquam quam rerum veniam.</p>
-									</div>
-									<h4 class="d-block">Gopal</h4>
-								</div>
-								<div class="col-5 ml-5 mr-auto">
-									<div class="text_box">
-										<p>Alan I love the topics you're covering, and I love the convenience of the video library. I've been doing the exercises for just a few days, and I can tell you I am already becoming more self-aware. I know I have a lot of negative self-beliefs to overcome, but I definitely feel like this is working - and it's only been a few days. I can't wait to see where my life will be in the future!</p>
-									</div>
-									<h4 class="d-block">Billy</h4>
-								</div>
-							</article>
-						</div>
-						<div class="carousel-item">
-							<article class="row d-flex">
-								<div class="col-5 mr-5 ml-auto">
-									<div class="text_box">
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi error magnam nulla, omnis porro velit vero vitae! Asperiores distinctio impedit nam numquam odio quaerat sint veniam. Aliquam quam rerum veniam.</p>
-									</div>
-									<h4 class="d-block">Gopal</h4>
-								</div>
-								<div class="col-5 ml-5 mr-auto">
-									<div class="text_box">
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi error magnam nulla, omnis porro velit vero vitae! Asperiores distinctio impedit nam numquam odio quaerat sint veniam. Aliquam quam rerum veniam.</p>
-									</div>
-									<h4 class="d-block">Gopal</h4>
-								</div>
-							</article>
-						</div>
+		<?php endwhile; ?>
+	<?php endif; ?>
+
+
+	<?php
+	wp_reset_postdata();
+
+	endif; ?>
+
+<?php
+
+$videoTextSlider = get_field('video_text_slider');
+
+if($videoTextSlider) :
+
+	?>
+	<?php if ( have_rows( 'video_text_slider' ) ) : ?>
+
+	<?php while ( have_rows( 'video_text_slider' ) ) : the_row(); ?>
+
+		<section class="row video_text_slider mb-5">
+			<div class="col-12">
+				<div class="container">
+
+					<div id="carouselVideoText" class="carousel slide carousel-fade" data-ride="carousel">
+
+						<?php if ( have_rows( 'slides' ) ) :
+							$count = 0;
+
+							//$slides = get_sub_field('slides');
+							//$rowCount = count($slides);
+							//echo $rowCount;
+							?>
+
+
+							<div class="carousel-inner">
+
+								<?php while ( have_rows( 'slides' ) ) : the_row(); $count++; ?>
+
+									<div class="carousel-item <?php if($count == 1) echo 'active'; ?>">
+										<article class="row">
+											<div class="col-12 col-md-6">
+												<?php
+												$videoLink = get_sub_field( 'slider_video_link' );
+
+												if ( strpos( $videoLink, "v=" ) ) {
+													$str       = explode( "v=", $videoLink );
+													$embedCode = preg_replace( '/\s+/', '', $str[1] );
+													$type      = "youtube";
+												} elseif ( strpos( $videoLink, "embed/" ) ) {
+													$str       = explode( "embed/", $videoLink );
+													$embedCode = preg_replace( '/\s+/', '', $str[1] );
+													$type      = "youtube";
+												} elseif ( strpos( $videoLink, "youtu.be" ) ) {
+													$str       = explode( ".be/", $videoLink );
+													$embedCode = preg_replace( '/\s+/', '', $str[1] );
+													$type      = "youtube";
+												} elseif ( strpos( $videoLink, "vimeo" ) !== false ) {
+													$str       = explode( "vimeo.com/", $videoLink );
+													$embedCode = preg_replace( '/\s+/', '', $str[1] );
+													$type      = "vimeo";
+												}
+
+												$attachment_id =  get_sub_field('slider_video_image');
+												$size = "video-thumb";
+												$videoImage = wp_get_attachment_image_src( $attachment_id, $size );
+
+												if (!empty($videoImage)) :
+													?>
+
+													<img src="<?php echo $videoImage[0]; ?>" alt="video image placeholder">
+
+												<?php elseif ($type == "youtube") : ?>
+
+													<img src="https://img.youtube.com/vi/<?php echo $embedCode; ?>/mqdefault.jpg" alt="youtube video thumbnail">
+
+												<?php else : ?>
+
+													<img src="<?php echo bloginfo('template_url'); ?>/images/video-placeholder.jpg" alt="video image placeholder"/>
+
+												<?php endif; ?>
+
+												<div class="play_button_wrap video_open position-absolute w-100 h-100 d-flex justify-content-center align-content-center" data-video="<?php echo $videoLink; if ($type == "vimeo") echo '/?autoplay=1'; else echo '/?rel=0&showinfo=0&autoplay=1'; ?>">
+													<div class="img_wrap m-auto">
+														<img class="play_button m-auto video_open_img" src="<?php echo bloginfo( 'template_url' ); ?>/images/icon-play.png"/>
+													</div>
+												</div>
+											</div>
+											<div class="col-12 col-md-6">
+												<p>"<?php the_sub_field('slider_text');?>"</p>
+												<h4><?php the_sub_field('persons_name');?></h4>
+											</div>
+										</article>
+									</div><!-- carousel-item" -->
+
+								<?php endwhile; ?>
+								<ol class="carousel-indicators">
+									<?php
+									for($x = 0; $x < $count; $x++) {
+										echo "<li data-target='#carouselVideoText' data-slide-to='$x'"; if( $x == 0) { echo " class='active'"; } echo "></li>";
+									}
+									?>
+								</ol>
+							</div><!-- carousel-inner -->
+						<?php endif; ?>
+					</div><!-- carouselVideoText  -->
+				</div><!-- container -->
+			</div><!-- col-12 -->
+		</section>
+
+	<?php endwhile; ?>
+
+<?php endif; ?>
+
+	<?php
+	wp_reset_postdata();
+endif; ?>
+
+<?php if ( have_rows( 'video_only' ) ) : ?>
+	<?php while ( have_rows( 'video_only' ) ) : the_row(); ?>
+
+		<section class="row full_width_video mb-5 px-5">
+			<div class="col-12 col-md-6 mx-auto">
+				<?php
+				$videoLink = get_sub_field( 'video_link' );
+
+				if ( strpos( $videoLink, "v=" ) ) {
+					$str       = explode( "v=", $videoLink );
+					$embedCode = preg_replace( '/\s+/', '', $str[1] );
+					$type      = "youtube";
+				} elseif ( strpos( $videoLink, "embed/" ) ) {
+					$str       = explode( "embed/", $videoLink );
+					$embedCode = preg_replace( '/\s+/', '', $str[1] );
+					$type      = "youtube";
+				} elseif ( strpos( $videoLink, "youtu.be" ) ) {
+					$str       = explode( ".be/", $videoLink );
+					$embedCode = preg_replace( '/\s+/', '', $str[1] );
+					$type      = "youtube";
+				} elseif ( strpos( $videoLink, "vimeo" ) !== false ) {
+					$str       = explode( "vimeo.com/", $videoLink );
+					$embedCode = preg_replace( '/\s+/', '', $str[1] );
+					$type      = "vimeo";
+				}
+
+				$attachment_id =  get_sub_field('video_image');
+				$size = "video-thumb";
+				$videoImage = wp_get_attachment_image_src( $attachment_id, $size );
+
+				if (!empty($videoImage)) :
+					?>
+
+					<img src="<?php echo $videoImage[0]; ?>" alt="video image placeholder">
+
+				<?php elseif ($type == "youtube") : ?>
+
+					<img src="https://img.youtube.com/vi/<?php echo $embedCode; ?>/maxresdefault.jpg" alt="youtube video thumbnail">
+
+				<?php else : ?>
+
+					<img src="<?php echo bloginfo('template_url'); ?>/images/video-placeholder.jpg" alt="video image placeholder"/>
+
+				<?php endif; ?>
+
+				<div class="play_button_wrap video_open position-absolute w-100 h-100 d-flex justify-content-center align-content-center" data-video="<?php echo $videoLink; if ($type == "vimeo") echo '/?autoplay=1'; else echo '/?rel=0&showinfo=0&autoplay=1'; ?>">
+					<div class="img_wrap m-auto">
+						<img class="play_button m-auto video_open_img" src="<?php echo bloginfo( 'template_url' ); ?>/images/icon-play.png"/>
 					</div>
-					<a class="carousel-control-prev" href="#carouselText" role="button" data-slide="prev">
-						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-						<span class="sr-only">Previous</span>
-					</a>
-					<a class="carousel-control-next" href="#carouselText" role="button" data-slide="next">
-						<span class="carousel-control-next-icon" aria-hidden="true"></span>
-						<span class="sr-only">Next</span>
-					</a>
 				</div>
 			</div>
-		</div>
-	</section>
+		</section>
 
-	<section class="row full_width_video">
-		<div class="col-12">
-			<img class="video_open" src="<?php echo bloginfo( 'template_url' ); ?>/images/testimonials-video-placeholer.jpg" data-video="https://www.youtube.com/watch?v=GjeRrJljrHs&list=RDGjeRrJljrHs"/>
-		</div>
-	</section>
+	<?php endwhile; ?>
+<?php endif; ?>
+
+<?php wp_reset_postdata();?>
 
 <?php get_footer(); ?>
